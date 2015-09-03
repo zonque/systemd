445ba0af70b95c3fe50567e3c344f614db98c07c
Daniel Mack <daniel@zonque.org>
2015-09-03 12:01:07 +0200

    resolved: make negative cache entries for NSEC records
    NSEC records are used in mDNS in order to signal non-existant resources.
    Honour that, and make negative cache entries in such cases.

--- src/resolve/resolved-dns-cache.c
+++ src/resolve/resolved-dns-cache.c
@@ -457,9 +457,18 @@ int dns_cache_put(
                 if (rr->key->cache_flush)
                         dns_cache_remove(c, rr->key);
 
-                r = dns_cache_put_positive(c, rr, timestamp, owner_family, owner_address);
-                if (r < 0)
-                        goto fail;
+                switch (rr->key->type) {
+                case DNS_TYPE_NSEC:
+                        r = dns_cache_put_negative(c, rr->key, rcode, timestamp, 0, owner_family, owner_address);
+                        if (r < 0)
+                                goto fail;
+                        break;
+                default:
+                        r = dns_cache_put_positive(c, rr, timestamp, owner_family, owner_address);
+                        if (r < 0)
+                                goto fail;
+                        break;
+                }
         }
 
         if (!key)
